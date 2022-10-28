2ac55d46cf97c5cfb057301e4ccc28a98c1dc922
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-10-28 18:17:02 +0600

    9

---
+++ src/component/redux/calculatorRedux/calculator.action.js
@@ -0,0 +1,31 @@
+import { LOAD_ANS, LOAD_CLEAR, LOAD_BUTTONS, LOAD_BACKSPACE } from '../calculatorRedux/calculator.actionType.js';
+import { number } from 'prop-types';
+
+
+export const LoadButtons = (number) => {
+    return {
+        type: LOAD_BUTTONS,
+        payload: number
+    }
+}
+
+export const LoadAns = (number) => {
+    return {
+        type: LOAD_ANS,
+        payload: number
+    }
+}
+
+export const LoadClear = (number) => {
+    return {
+        type: LOAD_CLEAR,
+        payload: number
+    }
+}
+
+export const LoadBackSpace = (number) => {
+    return {
+        type: LOAD_BACKSPACE,
+        payload: number
+    }
+}
