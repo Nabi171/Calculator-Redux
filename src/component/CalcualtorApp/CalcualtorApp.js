9be5594a214aa2faeb9e2a3e139ff4b4cae314f5
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-10-28 16:08:02 +0600

    6th

--- src/component/CalcualtorApp/CalcualtorApp.js
+++ src/component/CalcualtorApp/CalcualtorApp.js
@@ -6,24 +6,32 @@ const CalcualtorApp = () => {
             <div>
                 <input type="text"
                     placeholder="0"
-                    className="w-100 h-100 border-1  bg-black text-light fw-bold text-end pe-5"
+                    className="w-100 h-100 rounded border-1 p-5 bg-white text-black fw-bold text-end pe-5"
                 />
             </div>
-            <div>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
+            <div className='row mt-3'>
+                {/* <div className='col-lg-4'> */}
+                <button className='text-black  w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>.</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>C</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>/</button>
+                <button className='text-black w-25 h-50 rounded fw-bold fs-2 justify-content-center align-items-center  d-flex shadow'>+</button>
 
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>2</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>3</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>-</button>
 
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
-                <button className='text-white w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>1</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>4</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>5</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>6</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>*</button>
+
+                <button className='text-black w-25  rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>7</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>8</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>9</button>
+                <button className='text-black w-25 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>0</button>
+                <button className='text-black w-100 h-25 rounded fw-bold fs-2 justify-content-center align-items-center d-flex shadow'>AC</button>
+                {/* </div> */}
             </div>
         </div>
     );
