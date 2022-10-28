68c482c616a63f4699ba22ce8ffe7e1b56a9b980
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-10-28 14:20:56 +0600

    3rd

--- src/component/CalcualtorApp/CalcualtorApp.js
+++ src/component/CalcualtorApp/CalcualtorApp.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const CalcualtorApp = () => {
     return (
-        <div>
+        <div className='mt-2'>
             <div>
                 <input type="text"
                     placeholder="0"
-                    className="w-100 h-100"
+                    className="w-100 h-100 border-1  bg-black text-light fw-bold text-end pe-5"
                 />
             </div>
         </div>
